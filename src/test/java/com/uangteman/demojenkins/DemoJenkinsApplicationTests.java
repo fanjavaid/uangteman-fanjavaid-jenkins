78065df9dafe820c0ed95adc46d967dd1ba796e8
Fandi Akhmad <fanjavaid@fanjavaid-mbp.local>
2017-10-20 10:42:13 +0700

    Add sample Unit Test

--- src/test/java/com/uangteman/demojenkins/DemoJenkinsApplicationTests.java
+++ src/test/java/com/uangteman/demojenkins/DemoJenkinsApplicationTests.java
@@ -1,16 +1,25 @@
 package com.uangteman.demojenkins;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.uangteman.demojenkins.service.CalculationService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoJenkinsApplicationTests {
 
+	@Autowired
+	private CalculationService calculationService;
+	
 	@Test
 	public void contextLoads() {
+		assertEquals(20, calculationService.add(4, 20));
 	}
 
 }
